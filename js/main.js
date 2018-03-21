93f1b7827e2d99ff24f8f233190abda314461b90
Vernon <j_vernon3@tss-hr420-dm37.fc.ca>
2018-03-21 15:38:39 -0400

    created play pause and rewind buttons. unmuted video when mouse hovers the element

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 (() => {
   console.log('video started');
+
+  const vidPlayer = document.querySelector('video'),
+        playButton = document.querySelectorAll('button')[0],
+        pauseButton = document.querySelectorAll('button')[1];
+        rewindButton = document.querySelectorAll('button')[2];
+
+  function volOn() {
+    vidPlayer.muted = false;
+  }
+
+  function volOff() {
+    vidPlayer.muted = true;
+  }
+
+  function rewindVid() {
+    vidPlayer.currentTime -= 5;
+  }
+
+  function playVideo() {
+    vidPlayer.play();
+  }
+
+  function pauseVideo() {
+    vidPlayer.pause();
+  }
+
+  vidPlayer.addEventListener('mouseover', volOn);
+  vidPlayer.addEventListener('mouseout', volOff);
+  playButton.addEventListener('click', playVideo);
+  pauseButton.addEventListener('click', pauseVideo);
+  rewindButton.addEventListener('click', rewindVid);
+
 })();
